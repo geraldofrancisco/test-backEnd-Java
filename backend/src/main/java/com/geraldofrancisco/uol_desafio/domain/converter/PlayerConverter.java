f7cbcfb7e325af7138f79a57f751477de90f5960
Geraldo Francisco Neto <geraldof.neto2016@gmail.com>
2025-01-02 18:33:47 -0300

    serviço funcionando

---
+++ backend/src/main/java/com/geraldofrancisco/uol_desafio/domain/converter/PlayerConverter.java
@@ -0,0 +1,31 @@
+package com.geraldofrancisco.uol_desafio.domain.converter;
+
+import com.geraldofrancisco.uol_desafio.domain.db.Player;
+import com.geraldofrancisco.uol_desafio.domain.dto.player.PlayerDTO;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PlayerConverter {
+
+  public static Player toDomain(PlayerDTO dto) {
+    return Player.builder()
+        .email(dto.getEmail())
+        .name(dto.getName())
+        .codename(dto.getCodename())
+        .type(dto.getType())
+        .build();
+  }
+
+  public static PlayerDTO toDTO(Player domain) {
+    return PlayerDTO.builder()
+            .id(domain.getId())
+            .phone(domain.getPhone())
+            .creationDate(domain.getCreationDate())
+            .email(domain.getEmail())
+            .codename(domain.getCodename())
+            .type(domain.getType())
+            .name(domain.getName())
+            .build();
+  }
+}
--- backend/src/main/java/com/geraldofrancisco/uol_desafio/domain/service/PlayerService.java
+++ backend/src/main/java/com/geraldofrancisco/uol_desafio/domain/service/PlayerService.java
@@ -1,5 +1,6 @@
 package com.geraldofrancisco.uol_desafio.domain.service;
 
+import com.geraldofrancisco.uol_desafio.domain.converter.PlayerConverter;
 import com.geraldofrancisco.uol_desafio.domain.dto.player.PlayerDTO;
 import com.geraldofrancisco.uol_desafio.repository.PlayerRepository;
 import com.geraldofrancisco.uol_desafio.repository.integration.AvengerIntegration;
@@ -7,6 +8,7 @@ import com.geraldofrancisco.uol_desafio.repository.integration.JusticeLeagueInte
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -14,29 +16,62 @@ import java.util.List;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class PlayerService {    
-    
-    private final PlayerRepository repository;
-    private final AvengerIntegration avengerIntegration;
-    private final JusticeLeagueIntegration justiceLeagueIntegration;
+public class PlayerService {
 
+  private final PlayerRepository repository;
+  private final AvengerIntegration avengerIntegration;
+  private final JusticeLeagueIntegration justiceLeagueIntegration;
 
-    public Mono<PlayerDTO> create(PlayerDTO dto) {
-        switch (dto.getType()) {
-            case JUSTICE_LEAGUE -> {
-                return justiceLeagueIntegration.get()
-                        .flatMap(list -> createPlayer(list, dto));
-            }
-            case AVENGERS -> {
-                return avengerIntegration.get()
-                        .flatMap(list -> createPlayer(list, dto));
-            }
-        }
-
-        return Mono.empty();
+  public Mono<PlayerDTO> create(PlayerDTO dto) {
+    switch (dto.getType()) {
+      case JUSTICE_LEAGUE -> {
+        return justiceLeagueIntegration.get().flatMap(list -> createPlayer(list, dto));
+      }
+      case AVENGERS -> {
+        return avengerIntegration.get().flatMap(list -> createPlayer(list, dto));
+      }
     }
 
-    private Mono<PlayerDTO> createPlayer (List<String> heroes, PlayerDTO dto) {
-        return Mono.just(dto);
-    }
+    return Mono.empty();
+  }
+
+  private Mono<PlayerDTO> createPlayer(List<String> heroes, PlayerDTO dto) {
+    return repository
+        .findByType(dto.getType())
+        .collectList()
+        .flatMap(
+            previouslySaved ->
+                Mono.just(heroes)
+                    .filter(h -> h.size() > previouslySaved.size())
+                    .switchIfEmpty(
+                        Mono.error(
+                            new Exception(
+                                "Não existe espaço para esse tipo"))) // TODO: CRIAR EXCEÇÃO
+                    // PERSONALIZADA
+                    .flatMapMany(Flux::fromIterable)
+                    .filter(
+                        hero ->
+                            previouslySaved.stream()
+                                .noneMatch(
+                                    player -> player.getCodename().equals(dto.getCodename())))
+                    .switchIfEmpty(Mono.error(new Exception("usuário já cadastrado")))
+                    .filter(
+                        hero ->
+                            previouslySaved.stream()
+                                .noneMatch(player -> player.getCodename().equals(hero)))
+                    .next()
+        )
+        .map(hero -> {
+              dto.setCodename(hero);
+              return dto;
+        })
+        .flatMap(this::save);
+  }
+
+  private Mono<PlayerDTO> save(PlayerDTO dto) {
+    return Mono.just(dto)
+        .map(PlayerConverter::toDomain)
+        .flatMap(repository::save)
+        .map(PlayerConverter::toDTO);
+  }
 }
